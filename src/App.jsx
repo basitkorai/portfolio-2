9abcdad9e1a25552c4a89fdf49bfe5eb2f7181f2
basitkorai <basitalikorai7@gmail.com>
2024-02-06 17:40:18 +0500

    remove social list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Main from './components/Main'
 import Navbar from './components/Navbar'
-import SocialList from './components/SocialList'
 import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="container">
-      <SocialList />
       <Navbar />
       <Main />
       <Footer />
